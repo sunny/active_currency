e9d7bfbe60517aafb877aba6f9e935c3928339a1
Sunny Ripert <sunny@sunfox.org>
2018-09-14 00:19:12 +0200

    Remove fixtures

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,7 @@
 require "pry"
 require "timecop"
 
+# Load migrations
 require File.expand_path("../../spec/dummy/config/environment.rb", __FILE__)
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../spec/dummy/db/migrate", __FILE__)]
 ActiveRecord::Migrator.migrations_paths << File.expand_path('../../db/migrate', __FILE__)
-
-# Load fixtures from the engine
-if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
-  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
-  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
-  ActiveSupport::TestCase.fixtures :all
-end
