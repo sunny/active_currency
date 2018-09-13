6bfb0b91cba8e9d91dbd60ec64e938e5a0e454b3
Sunny Ripert <sunny@sunfox.org>
2018-09-14 00:35:25 +0200

    Share migrations

--- db/migrate/20180911202100_create_active_currency_rates.rb
+++ db/migrate/20180911202100_create_active_currency_rates.rb
@@ -1,4 +1,4 @@
-class CreateActiveCurrencyRates < ActiveRecord::Migration[5.1]
+class CreateActiveCurrencyRates < ActiveRecord::Migration[5.0]
   def change
     create_table :active_currency_rates do |t|
       t.string :from
--- lib/active_currency/engine.rb
+++ lib/active_currency/engine.rb
@@ -1,5 +1,15 @@
 module ActiveCurrency
   class Engine < ::Rails::Engine
     isolate_namespace ActiveCurrency
+
+    initializer :append_migrations do |app|
+      # This prevents migrations from being loaded twice from the inside of the
+      # gem itself (dummy test app)
+      if app.root.to_s !~ /#{root}/
+        config.paths['db/migrate'].expanded.each do |migration_path|
+          app.config.paths['db/migrate'] << migration_path
+        end
+      end
+    end
   end
 end
