43200dac8770c877831c8c77db19aa65f664d59e
Sunny Ripert <sunny@sunfox.org>
2018-10-12 12:55:57 +0200

    Refactor add rates

--- lib/active_currency/add_rates.rb
+++ lib/active_currency/add_rates.rb
@@ -8,16 +8,8 @@ module ActiveCurrency
     end
 
     def call
-      bank = EuCentralBank.new
-      bank.update_rates
-
-      from = 'EUR'
-      currencies.map(&:to_s).each do |to|
-        next if to == from
-
-        rate = bank.get_rate(from, to)
-        store.add_rate(from, to, rate)
-        store.add_rate(to, from, 1.fdiv(rate))
+      other_currencies.each do |to|
+        store_rate(from, to)
       end
     end
 
@@ -31,8 +23,31 @@ module ActiveCurrency
       @currencies.map(&:to_s).map(&:upcase)
     end
 
+    def other_currencies
+      currencies - [from]
+    end
+
+    def from
+      'EUR'
+    end
+
     def store
       @store ||= ActiveCurrency::RateStore.new
+    end
+
+    def bank
+      @bank ||= EuCentralBank.new.tap(&:update_rates)
+    end
+
+    def store_rate(from, to)
+      rate = bank.get_rate(from, to)
+
+      if rate.nil? || rate.zero?
+        raise "Bank rate must be set but bank returned #{rate.inpsect}"
+      end
+
+      store.add_rate(from, to, rate)
+      store.add_rate(to, from, 1.fdiv(rate))
     end
   end
 end
