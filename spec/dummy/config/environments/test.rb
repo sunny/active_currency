86dc29d155c6abec083bc783661275b7aa11beba
Sunny Ripert <sunny@sunfox.org>
2019-11-05 21:41:52 +0100

    Remove Rails3.2-specific configuration

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -15,14 +15,6 @@ Dummy::Application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
-  if ENV['BUNDLE_GEMFILE'] == 'Gemfile-rails3.2'
-    # Configure public file server for tests with Cache-Control for performance.
-    config.public_file_server.enabled = true
-    config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{1.hour.seconds.to_i}"
-    }
-  end
-
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
