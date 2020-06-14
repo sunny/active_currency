f7933a42cda61db4f44a2399d9a1c2ea3f893e8a
Sunny Ripert <sunny@sunfox.org>
2020-06-14 13:36:05 +0200

    Remove deprecation notice

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -13,7 +13,7 @@ module Dummy
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1 if config.respond_to?(:load_defaults)
 
-    if config.active_record.sqlite3.respond_to?(:represent_boolean_as_integer)
+    if Gem::Version.new(Rails.version) <= Gem::Version.new(6)
       config.active_record.sqlite3.represent_boolean_as_integer = true
     end
   end
