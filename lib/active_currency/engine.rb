cae814fe1495a2c23bfd99a953f335e99b7bd96a
Anne-Sophie Rouaux <rouaux.annesophie@gmail.com>
2020-09-08 17:11:34 +0200

    The never ending story of engine migrations (#16)

--- lib/active_currency/engine.rb
+++ lib/active_currency/engine.rb
@@ -7,7 +7,7 @@ module ActiveCurrency
     initializer :append_migrations do |app|
       next if app.root.to_s.match(root.to_s)
 
-      paths = ActiveRecord::Migrator.migrations_paths
+      paths = ActiveRecord::Tasks::DatabaseTasks.migrations_paths
       config.paths['db/migrate'].expanded.each do |path|
         paths << path
       end
